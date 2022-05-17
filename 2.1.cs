5c0a63dc2b287899583e62a9038c33e7cef61853
kovkat <105742967+kovkat@users.noreply.github.com>
2022-05-17 22:37:38 +0300

    Update 2.1.cs

--- 2.1.cs
+++ 2.1.cs
@@ -3,7 +3,6 @@ namespace Laba2._1
 {
     class Program
     {
-
         public class Piano
         {
             public static System.Collections.ArrayList piano = new System.Collections.ArrayList();
